f0b4e70ad93a9b41cc1c75470d0eab789d1501ed
= <gregsherrid@gmail.com>
2019-07-11 18:55:26 +0000

    server runs

--- server.py
+++ server.py
@@ -7,3 +7,7 @@ CORS(app)
 @app.route("/")
 def home():
 	return "Hello, TF Embeddings!"
+
+if __name__ == "__main__":
+        # Only for debugging while developing
+        app.run(host='0.0.0.0', debug=True, port=3333)
